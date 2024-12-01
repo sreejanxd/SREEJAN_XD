6ce475f58f2ae7ea29bc97407531a3d4417e406c
Sreejanxd🥵 <oyysreejan@gmail.com>
2024-12-01 19:57:00 +0530

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -6,7 +6,7 @@ cmd({
     desc: "jarvis-md support",
     type: "support"
 }, async (message) => {
-    const name = ' ─꯭⃜⃜͢͢🌸꯭͟🇸𝖗𝖊𝖊𝖏𝖆𝖓།꯭͓⎯꯭ͯ𓆗🎓', title = "ᴊᴀʀᴠɪꜱ ꜱᴜᴩᴩᴏʀᴛ 🪄", number = '917439382677', body = "ɪʀᴏɴ ᴍᴀɴ";
+    const name = ' ─꯭⃜⃜͢͢🌸꯭͟🇸𝖗𝖊𝖊𝖏𝖆𝖓།꯭͓⎯꯭ͯ𓆗🎓', title = "》✪⏤͟͞★⃝ꪶ‎𝐒𝐫𝖊𝖊⊭𝖏ꫝ𝖓-𖥘✪͜͡➺🪄", number = '917439382677', body = "🎀🩵 𝐋ɪғᴇ 𝐒ᴜᴄᴋs-𝐊ʀɪsʜɴᴀ 𝐇ᴇᴀʟs 🌸🦚";
     const image = "https://graph.org/file/58ea74675af7836579a3a.jpg", sourceUrl = 'https://github.com/sreejanxd';
     const logo = await getBuffer(image);
     const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: powered by sreejan-xd;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
