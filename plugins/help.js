b7c8d48f3344594d7dba4f3bcb78d0ac8633e0b7
Sreejanxd🥵 <oyysreejan@gmail.com>
2024-12-01 19:58:30 +0530

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -3,11 +3,11 @@ const {cmd , commands} = require('../command')
 cmd({
   pattern: "help",
     fromMe: isPrivate,
-    desc: "jarvis-md support",
+    desc: "sreejan-xd support",
     type: "support"
 }, async (message) => {
     const name = ' ─꯭⃜⃜͢͢🌸꯭͟🇸𝖗𝖊𝖊𝖏𝖆𝖓།꯭͓⎯꯭ͯ𓆗🎓', title = "》✪⏤͟͞★⃝ꪶ‎𝐒𝐫𝖊𝖊⊭𝖏ꫝ𝖓-𖥘✪͜͡➺🪄", number = '917439382677', body = "🎀🩵 𝐋ɪғᴇ 𝐒ᴜᴄᴋs-𝐊ʀɪsʜɴᴀ 𝐇ᴇᴀʟs 🌸🦚";
-    const image = "https://graph.org/file/58ea74675af7836579a3a.jpg", sourceUrl = 'https://github.com/sreejanxd';
+    const image = "https://cdn.ironman.my.id/i/abdg5l.jpg", sourceUrl = 'https://github.com/sreejanxd';
     const logo = await getBuffer(image);
     const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: powered by sreejan-xd;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
     const adon = { title, body, thumbnail: logo, mediaType: 1, mediaUrl: sourceUrl, sourceUrl, showAdAttribution: true, renderLargerThumbnail: false };
