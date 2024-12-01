5002ee090ab7dea551ed4a55ac9a3088cbfcfd20
Sreejanxd🥵 <oyysreejan@gmail.com>
2024-12-01 15:06:52 +0530

    Create vv.js

---
+++ plugins/vv.js
@@ -0,0 +1,10 @@
+System({
+    pattern: "vv",
+    fromMe: true,
+    type: "tool",
+    alias: ['view'],
+    desc: "get view ones message"
+}, async (message) => {
+   if (!message.reply_message.viewones) return await message.reply("_*Reply to a view once*_");
+   return await message.client.forwardMessage(message.chat, message.reply_message.message, { readViewOnce: true });
+});
