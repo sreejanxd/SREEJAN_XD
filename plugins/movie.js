61167b80851f2ffc0e7d010fc54ae5f8b801afcb
Sreejanxd🥵 <oyysreejan@gmail.com>
2024-12-01 20:02:26 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -50,7 +50,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         // Send the movie information along with the poster image
         await conn.sendMessage(from, {
             image: { url: imageUrl },
-            caption: `${movieInfo}\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`
+            caption: `${movieInfo}\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ  ─꯭⃜⃜͢͢🌸꯭͟🇸𝖗𝖊𝖊𝖏𝖆𝖓།꯭͓⎯꯭ͯ𓆗`
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
